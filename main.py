9356bc70ffa3e6860632598baa8361135e358ed4
Eamonn <eamonn.m.r@gmail.com>
2017-02-27 12:12:08 -0500

    Proper support for multithreading on the server

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ class Server():
         while True:
             (clientsock, address) = self.socket.accept()
             print(address)
-            ServerThread(clientsock).run()
+            ServerThread(clientsock).start()
 
 
 class ServerThread(threading.Thread):
     def __init__(self, client_socket):
+        threading.Thread.__init__(self)
         self.socket = client_socket
 
     def run(self):
@@ -35,6 +36,7 @@ class ServerThread(threading.Thread):
             last_data = self.socket.recv(length)
             print(last_data)
 
+
 class Client():
     def __init__(self, host, port):
         self.socket = get_socket()
@@ -42,6 +44,7 @@ class Client():
 
     def send(self, string):
         self.socket.send(string)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs OpenLockstep')
