b841d960da30ae5832c2b2d2b57f188193d8f894
Eamonn <eamonn.m.r@gmail.com>
2017-02-28 10:42:05 -0500

    Detect clicks
    This adds pygame to the project in a more real way - it loads settings,
    opens a window, and detects clicks.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import argparse
-
+import pygame
+import json
 import net
 
 if __name__ == "__main__":
@@ -11,15 +12,32 @@ if __name__ == "__main__":
     parser.add_argument('--port', type=int, default=8000)
     parser.add_argument('--host', type=str, default='localhost')
     parser.add_argument('--clients', type=int, default=1)
+    parser.add_argument('--settings_file', type=str, default='settings.json')
+    parser.add_argument('--settings', type=str, default='{}')
 
     args = parser.parse_args()
+
+    settings = json.load(open(args.settings_file))
+    settings.update(json.loads(args.settings))
+
     if args.client:
+        screen = pygame.display.set_mode(settings['screen_size'])
+        pygame.display.set_caption("OpenLockstep RTS")
         client = net.Client(args.host, args.port)
+        mousedown = False
+
         while True:
-            text = input("> ")[:255]
-            client.send(bytes([len(text)]))
-            client.send(text.encode('utf-8'))
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and mousedown:
+                    print('click')
+                    mousedown = False
+                elif event.type == pygame.MOUSEBUTTONDOWN and not mousedown:
+                    mousedown = True
+
+        #while True:
+        #    text = input("> ")[:255]
+        #    client.send(bytes([len(text)]))
+        #    client.send(text.encode('utf-8'))
     elif args.server:
         net.Server(args.port, host=args.host, client_count=args.clients).run()
-
 
