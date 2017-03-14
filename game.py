3a79a333f9e7435ad53d078c2813ef0ee6d2130b
Eamonn <eamonn.m.r@gmail.com>
2017-03-13 22:14:33 -0400

    Refactor game to a more OO state
    This moves most of the game loop variables into instance vars and
    splits logical parts of the game loop out into functions.

--- game.py
+++ game.py
@@ -16,31 +16,42 @@ class Game:
         self.screen = pygame.display.set_mode(settings['screen_size'])
         pygame.display.set_caption("OpenLockstep RTS")
         self.client = net.Client(args.host, args.port)
+        self.step = None
+        self.command_list = None
+        self.mousedown = 0
 
-    def start(self): 
-        mousedown = False
-        step = 0
+    def start(self):
+        self.command_list = []
+        self.mousedown = False
+        self.step = 0
         pygame.time.set_timer(TIMER_EVENT, STEP_LENGTH)
-        command_list = [] 
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONUP and mousedown:
-                    command_list += [commands.Ping(position=event.pos)]
-                    mousedown = False
-                elif event.type == pygame.MOUSEBUTTONDOWN and not mousedown:
-                    mousedown = True
-                elif event.type == TIMER_EVENT:
-                    # Transmit accumulated commands then clear list
-                    self.client.send(step, command_list)
-                    command_list = [] # Set-to-new-empty, not delete
-
-                    # Wait for the server
-                    # See net for why this should not lag the game
-                    # TODO: Handle lag more gracefully (show "lag" screen?)
-                    in_step = self.client.block_until_get_step(step)
-                    # TODO: Game logic goes here
-                    for ping in in_step.commands:
-                        pygame.draw.circle(self.screen, (200, 200, 200),
-                                           ping.position, 10, 2)
-                    step += 1 # Only advance after we've recieved a new step
+                self.process_event(event)
             pygame.display.flip()
+
+    def process_event(self, event):
+        if event.type == pygame.MOUSEBUTTONUP and mousedown:
+            self.command_list += [commands.Ping(position=event.pos)]
+            self.mousedown = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and not mousedown:
+            mousedown = True
+        elif event.type == TIMER_EVENT:
+            self.advance_step()
+
+    def advance_step(self):
+        # Transmit accumulated commands then clear list
+        self.client.send(step, command_list)
+        self.command_list = [] # Set-to-new-empty, not delete
+
+        # Wait for the server
+        # See net for why this should not lag the game
+        # TODO: Handle lag more gracefully (show "lag" screen?)
+        self.execute_step(self.client.block_until_get_step(step))
+        # TODO: Game logic goes here
+        self.step += 1 # Only advance after we've recieved a new step
+
+    def execute_step(self, step):
+        for ping in in_step.commands:
+            # Dummy code to draw pings
+            pygame.draw.circle(self.screen, (200, 200, 200), ping.position, 10, 2)
