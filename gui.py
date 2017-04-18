b3a4663faf9445cb1b6cae25f6bcac718f245191
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2017-04-18 18:12:15 -0400

    Selection box drawing
    Creates a selection box between the mouse and the initial dragging
    location.

--- gui.py
+++ gui.py
@@ -46,9 +46,20 @@ class NormalMouse(MouseMode):
         self.parent = parent
         self.sprite = sprite
         self.dragging = False
+        self.initial_drag_pos = None
+        self.selection_box = None
 
     def draw(self):
         x, y = pygame.mouse.get_pos()
+
+
+        if self.dragging:
+            self._update_selection_box()
+            pygame.draw.rect(self.parent.screen,
+                             (200, 200, 250),
+                             self.selection_box, 
+                             1)
+
         self.sprite.draw(x=x, y=y,
                 frame=11 if self.dragging else 12, 
                 # TODO: Idea: sprites with named frames?
@@ -56,6 +67,16 @@ class NormalMouse(MouseMode):
     
     def up(self):
         self.dragging = False
+        self.selection_box = None # Select all units in this box
 
     def down(self):
         self.dragging = True
+        self.initial_drag_pos = pygame.mouse.get_pos()
+        self._update_selection_box()
+
+    def _update_selection_box(self):
+        x, y = self.initial_drag_pos
+        nx, ny = pygame.mouse.get_pos()
+        w = nx - x
+        h = ny - y
+        self.selection_box = pygame.Rect(x, y, w, h)
